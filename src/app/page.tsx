082d512b35fe0fb058cea58185241ec7f8148910
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-12-01 23:34:19 +0200

    add filtering behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,51 @@
-import { MovieCard, SearchBar } from "@/components";
+import { MovieCard, RatingFilter, SearchBar } from "@/components";
 import { MoviesResponse } from "@/types";
+
+async function fetchMovies(term?: string) {
+  try {
+    const MoviesRes = await fetch(
+      `https://www.freetestapi.com/api/v1/movies?search=${term}`
+    );
+
+    if (!MoviesRes.ok) {
+      throw new Error("Failed to fetch movies");
+    }
+
+    const MoviesData: MoviesResponse = await MoviesRes.json();
+    return MoviesData;
+  } catch (error) {
+    console.error("Failed to fetch movies:", error);
+  }
+}
+
 type HomePageProps = {
   searchParams: Promise<{
-    term: string;
+    term?: string;
+    rating?: string;
   }>;
 };
 export default async function page({ searchParams }: HomePageProps) {
   const searchTerm = await searchParams;
   const term = searchTerm.term ?? "";
-  const MoviesRes = await fetch(
-    `https://www.freetestapi.com/api/v1/movies?search=${term}`
-  );
-  const MoviesData: MoviesResponse = await MoviesRes.json();
-  const movieCardElement = MoviesData.map((movie) => {
+  const rating = searchTerm.rating ?? "";
+  const MoviesData: MoviesResponse | undefined = await fetchMovies(term);
+  const filteredMovies =
+    rating && rating !== "all"
+      ? MoviesData?.filter(
+          (movie) => Math.floor(movie.rating) === Number(rating)
+        )
+      : MoviesData;
+  const movieCardElement = filteredMovies?.map((movie) => {
     return <MovieCard movie={movie} key={movie.id} />;
   });
   return (
     <main>
-      <header className="mx-4">
+      <header className="mx-4 flex  items-center justify-between gap-4">
         <SearchBar />
+        <RatingFilter />
       </header>
       <section
-        className=" mt-10 px-10 grid justify-items-center gap-9 gap-y-6
+        className="mt-10 px-10 grid justify-items-center gap-9 gap-y-6
 lg:grid-cols-[repeat(auto-fill,minmax(140px,1fr))] xl:grid-cols-[repeat(auto-fill,minmax(150px,1fr))] grid-cols-[repeat(auto-fill,minmax(150px,1fr))]"
       >
         {movieCardElement}
