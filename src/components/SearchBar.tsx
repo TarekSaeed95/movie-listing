ee4c629fce49878c5a7983724272ea73bcbc9c41
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-12-01 20:08:25 +0200

    add fetching apis func and fix search bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,12 +9,12 @@ export const SearchBar = () => {
   const handleClick = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       inputRef?.current?.["value"] !== ""
-        ? router.push(`/term=${inputRef?.current?.["value"]}`)
+        ? router.push(`?term=${inputRef?.current?.["value"]}`)
         : router.push("/");
     }
   };
   return (
-    <div className="relative w-1/3 ">
+    <div className="relative w-full md:w-1/3 text-black mt-5">
       <MdOutlineSearch
         className={
           "absolute top-1/2 -translate-y-1/2 text-lg md:text-[25px] left-3"
@@ -25,7 +25,7 @@ export const SearchBar = () => {
         type="text"
         onKeyDown={(e) => handleClick(e)}
         className={
-          "input input-bordered md:input-lg h-9 w-full md:h-10 md:min-w-[419px] rounded-full outline-none placeholder:text-sm md:placeholder:text-lg placeholder:text-darkblue ps-8 md:ps-11 "
+          "input input-bordered md:input-lg h-9 w-full md:h-10 md:min-w-[419px] rounded-full outline-none placeholder:text-sm md:placeholder:text-lg  ps-8 md:ps-11 "
         }
         placeholder="Search"
       />
